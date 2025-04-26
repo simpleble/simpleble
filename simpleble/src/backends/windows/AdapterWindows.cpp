c8b40631461ff1ab63b95ecc0b662722494b6225
RoadRoller01 <roadroller01mail@proton.me>
2025-04-26 16:27:46 +0300

    Fix c++20 errors

--- simpleble/src/backends/windows/AdapterWindows.cpp
+++ simpleble/src/backends/windows/AdapterWindows.cpp
@@ -91,7 +91,7 @@ void AdapterWindows::scan_stop() {
     });
 
     std::unique_lock<std::mutex> lock(scan_stop_mutex_);
-    if (scan_stop_cv_.wait_for(lock, 1s, [=] { return !this->scan_is_active_; })) {
+    if (scan_stop_cv_.wait_for(lock, 1s, [=, this] { return !scan_is_active_; })) {
         // Scan stopped
     } else {
         // Scan did not stop, this can be because some other process
@@ -220,7 +220,7 @@ void AdapterWindows::_on_scanner_received(
 
     // Parse manufacturer data
     auto manufacturer_data = args.Advertisement().ManufacturerData();
-    for (auto& item : manufacturer_data) {
+    for (const auto& item : manufacturer_data) {
         uint16_t company_id = item.CompanyId();
         ByteArray manufacturer_data_buffer = ibuffer_to_bytearray(item.Data());
         data.manufacturer_data[company_id] = manufacturer_data_buffer;
@@ -269,7 +269,7 @@ void AdapterWindows::_on_scanner_received(
 
     // Parse service uuids
     auto service_data = args.Advertisement().ServiceUuids();
-    for (auto& service_guid : service_data) {
+    for (const auto& service_guid : service_data) {
         std::string service_uuid = guid_to_uuid(service_guid);
         data.service_data.emplace(std::make_pair(service_uuid, ByteArray()));
     }
