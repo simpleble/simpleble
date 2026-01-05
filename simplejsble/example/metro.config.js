0b27e0d254902f7ceb6947d5b079b819c41ba678
Alejo <alejoivanrojas@gmail.com>
2026-01-05 15:02:39 -0300

    update metro.config.js to adjust workspace root path

--- simplejsble/example/metro.config.js
+++ simplejsble/example/metro.config.js
@@ -3,7 +3,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const workspaceRoot = path.resolve(projectRoot, '..');
 const config = getDefaultConfig(projectRoot);
 
 
@@ -13,12 +13,12 @@ config.watchFolders = [workspaceRoot];
 config.resolver = {
     ...config.resolver,
     nodeModulesPaths: [
-        path.resolve(workspaceRoot, 'node_modules'),
         path.resolve(projectRoot, 'node_modules'),
+        path.resolve(workspaceRoot, 'node_modules'),
     ],
     extraNodeModules: {
         ...config.resolver.extraNodeModules,
-        'simplejsble': path.resolve(workspaceRoot),
+        'simplejsble': workspaceRoot,
     }
 }
 
