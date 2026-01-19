6df989b1a602be3aa0e37d4ca3ad2607ffdd2e8e
Alejo <alejoivanrojas@gmail.com>
2026-01-19 15:03:18 -0300

    rename references from Simplejsble to Simplernble

--- simplernble/packages/simplernble/app.plugin.js
+++ simplernble/packages/simplernble/app.plugin.js
@@ -2,6 +2,6 @@ const { withAndroidManifest } = require('@expo/config-plugins');
 
 // This plugin ensures Expo autolinking detects the package
 // The actual native module registration is handled by react-native.config.js
-module.exports = function withSimplejsble(config) {
+module.exports = function withSimplernble(config) {
   return config;
 };
--- simplernble/packages/simplernble/apple/Bridge.h
+++ simplernble/packages/simplernble/apple/Bridge.h
@@ -1,6 +1,6 @@
 //
 //  Bridge.h
-//  NitroSimplejsble
+//  NitroSimplernble
 //
 //  Created by Marc Rousavy on 22.07.24.
 //
