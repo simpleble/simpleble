a3f3dc91325c50b887420b1062432a71d792f7fb
Alejo <alejoivanrojas@gmail.com>
2026-01-20 22:59:28 -0300

    add missing hybrid classes for autolinking

--- simplernble/src/index.ts
+++ simplernble/src/index.ts
@@ -1,12 +1,16 @@
 import { NitroModules } from "react-native-nitro-modules";
-import type { Adapter } from "./specs/Adapter.nitro";
-import type { Peripheral, BluetoothAddressType } from "./specs/Peripheral.nitro";
-import type { Service } from "./specs/Service.nitro";
-import type { Characteristic } from "./specs/Characteristic.nitro";
-import type { Descriptor } from "./specs/Descriptor.nitro";
+import type { Adapter as AdapterInterface } from "./specs/Adapter.nitro";
+import type { Peripheral as PeripheralInterface, BluetoothAddressType } from "./specs/Peripheral.nitro";
+import type { Service as ServiceInterface } from "./specs/Service.nitro";
+import type { Characteristic as CharacteristicInterface } from "./specs/Characteristic.nitro";
+import type { Descriptor as DescriptorInterface } from "./specs/Descriptor.nitro";
 
-export type { Adapter, Peripheral, BluetoothAddressType, Service, Characteristic, Descriptor };
+export type { BluetoothAddressType };
 
 export { fromHex, toHex, toString, fromString } from "./utils/bytearray";
 
-export const HybridAdapter = NitroModules.createHybridObject<Adapter>("Adapter");
+export const Adapter = NitroModules.createHybridObject<AdapterInterface>("Adapter");
+export const Peripheral = NitroModules.createHybridObject<PeripheralInterface>("Peripheral");
+export const Service = NitroModules.createHybridObject<ServiceInterface>("Service");
+export const Characteristic = NitroModules.createHybridObject<CharacteristicInterface>("Characteristic");
+export const Descriptor = NitroModules.createHybridObject<DescriptorInterface>("Descriptor");
