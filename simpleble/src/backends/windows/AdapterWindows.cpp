4307f9c8b51b9253ae9dd7cd9d2af1da1b8cb800
Kevin Dewald <5274600+kdewald@users.noreply.github.com>
2026-08-03 22:30:39 -0700

    Add Windows extended advertisement scanning

--- simpleble/src/backends/windows/AdapterWindows.cpp
+++ simpleble/src/backends/windows/AdapterWindows.cpp
@@ -14,6 +14,7 @@
 #include "winrt/Windows.Devices.Radios.h"
 #include "winrt/Windows.Devices.Enumeration.h"
 #include "winrt/Windows.Foundation.Collections.h"
+#include "winrt/Windows.Foundation.Metadata.h"
 #include "winrt/Windows.Foundation.h"
 #include "winrt/base.h"
 
@@ -42,6 +43,11 @@ AdapterWindows::AdapterWindows(std::string device_id)
 
     // Configure the scanner object
     scanner_ = Advertisement::BluetoothLEAdvertisementWatcher();
+    if (Foundation::Metadata::ApiInformation::IsPropertyPresent(
+            L"Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher",
+            L"AllowExtendedAdvertisements")) {
+        scanner_.AllowExtendedAdvertisements(true);
+    }
 
     // Register member functions directly as callback handlers
     radio_state_changed_token_ = radio_.StateChanged({this, &AdapterWindows::on_power_state_changed});
